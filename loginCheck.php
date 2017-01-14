1d425b03834520ebd2cfd19e6346b976360b9ffd
hotorin <senshimaseira@gmail.com>
2017-01-15 01:02:22 +0700

    update md5 and html5 require

--- loginCheck.php
+++ loginCheck.php
@@ -4,31 +4,35 @@ session_start();
 
 //----------------------------------------------------------------------------------------------------------------------//
 if(isset($_POST['username']) && isset($_POST['password'])){
-		$q = 'SELECT * FROM member WHERE username = "'.$_POST['username'].'" AND password = "'.$_POST['password'].'";';
+		$q = 'SELECT * FROM member WHERE username = "'.$_POST['username'].'" AND password = "'.md5($_POST['password']).'";';
 		$res = $db -> query($q);
-		while($row = $res -> fetch_array())
-			{
-				$_SESSION['fname'] = $row['full_name'];
-				$_SESSION['user_name'] = $row['username'];
-				$_SESSION['user_pass'] = $row['password'];
-				$_SESSION['e_mail'] = $row['email'];
-				$_SESSION['tier'] = $row['member_tier'];
+		if ($res && $res->num_rows == 1 ){
+			while($row = $res -> fetch_array()){
+					$_SESSION['fname'] = $row['full_name'];
+					$_SESSION['user_name'] = $row['username'];
+					$_SESSION['user_pass'] = $row['password'];
+					$_SESSION['e_mail'] = $row['email'];
+					$_SESSION['tier'] = $row['member_tier'];
 			}
-}
-else{
-  echo "
-        <script type='text/javascript'>
-          alert('Log-in Failed, Please try again!');
-        </script>
-       ";
-  echo "
-        <script type='text/javascript'>
-          window.location = 'login.php';
-        </script>
-       ";
+		}
+		else{
+		  echo "
+		        <script type='text/javascript'>
+		          alert('Login Failed, Your username or password is invalid!');
+		        </script>
+		       ";
+		  echo "
+		        <script type='text/javascript'>
+		          window.location = 'login.php';
+		        </script>
+		       ";
+		}
 }
 //-----------------------------------------------------------------------------------------------------------------------//
 ?>
 <script type='text/javascript'>
-  window.location = '.';
+	alert('Login Success!');
+</script>
+<script type='text/javascript'>
+	window.location = '.';
 </script>
--- register.php
+++ register.php
@@ -35,23 +35,23 @@
 
     <form action="register_check.php" method="post">
       <div class="form-group has-feedback">
-        <input type="text" name="f_name" class="form-control" placeholder="Full name">
+        <input type="text" name="f_name" class="form-control" placeholder="Full name" required="required" pattern="[A-Za-z0-9]{1,20}">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="text" name="u_name" class="form-control" placeholder="Username">
+        <input type="text" name="u_name" class="form-control" placeholder="Username" required="required" pattern="[A-Za-z0-9]{1,20}">
         <span class="glyphicon glyphicon-user form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="email" name="e-mail" class="form-control" placeholder="Email">
+        <input type="email" name="e-mail" class="form-control" placeholder="Email" required="required" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{1,4}$">
         <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" name="p_word" class="form-control" placeholder="Password">
+        <input type="password" id="p_word" name="p_word" class="form-control" placeholder="Password must have 6-20 letters" required="required" pattern="[A-Za-z0-9]{6,20}">
         <span class="glyphicon glyphicon-lock form-control-feedback"></span>
       </div>
       <div class="form-group has-feedback">
-        <input type="password" name="re_p" class="form-control" placeholder="Retype password">
+        <input type="password" id="re_p" name="re_p" class="form-control" placeholder="Retype your password" required="required" pattern="[A-Za-z0-9]{6,20}">
         <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
       </div>
       <div class="row">
